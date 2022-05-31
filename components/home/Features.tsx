3ce0184b38ad4dc6d5646bd2e9ea758d90b0ca32
Zachary Myre <zmyre@securitybrandsinc.com>
2022-05-31 16:03:09 -0500

    Update Features.tsx

--- components/home/Features.tsx
+++ components/home/Features.tsx
@@ -5,14 +5,14 @@ import { MdSettingsBackupRestore } from "react-icons/md";
 
 export const Features = () => {
   return (
-    <div className="flex flex-col md:flex-row mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-      <h1 className="text-center rounded text-3xl tracking-tight font-bold text-gray-100">
-        <span className="block">Services for you.</span>
+    <div className="flex flex-col mt-10 mx-auto max-w-7xl px-4 sm:mt-8 sm:px-6 md:mt-16 lg:px-8">
+      <h1 className="text-center rounded my-8 text-4xl tracking-tight font-bold text-white md:text-5xl md:mb-5">
+        <span className="border-b-4 border-white px-16 pb-2 rounded-full shadow-white shadow-md">Services for you.</span>
       </h1>
-      <div className="space-y-6 mt-6">
+      <div className="space-y-6 mt-6 md:mt-12 md:space-y-0 md:grid md:gap-4 md:grid-cols-4">
           <div className="flex flex-row justify-between items-center rounded-xl p-5 bg-white shadow-cyan-600 shadow-lg">
             <ImStatsBars className="rounded w-12 h-12 text-cyan-400 bg-transparent " />
-            <h2 className="text-right text-black text-xl font-semibold bg-transparent">IT Consulting Strategies</h2>
+            <h2 className="text-right text-black text-xl font-semibold bg-transparent">IT Consulting & Strategies</h2>
           </div>
           <div className="flex flex-row justify-between items-center rounded-xl p-5 bg-white shadow-cyan-600 shadow-lg">
             <FaNetworkWired className="rounded w-12 h-12 text-cyan-400 bg-transparent " />
