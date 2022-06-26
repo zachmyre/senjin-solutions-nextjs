a205a18be8c4cab989c1400ddab5fd7d61a6f590
Zach Myre <myrezach@live.com>
2022-06-26 15:46:42 -0500

    Update sendgrid.ts

--- pages/api/sendgrid.ts
+++ pages/api/sendgrid.ts
@@ -1,7 +1,6 @@
 import sendgrid from "@sendgrid/mail";
-import { SENDGRID_API_KEY } from '../../shh';
 
-sendgrid.setApiKey(SENDGRID_API_KEY ?? process.env.SENDGRID_API_KEY);
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 async function sendEmail(req: any, res: any) {
   console.log(req.body);
